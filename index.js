20b8ba42725eccb42dca22a52762ab557a76de66
Betül <69481739+FatmaBetulUyar@users.noreply.github.com>
2023-08-19 00:38:50 +0300

    book adding information added and clear form after submit.

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const btnNewBook = document.querySelector(".newBook");
   const form = document.querySelector(".comment-form");
   const overlay = document.getElementById("overlay");
+  const overlayResult = document.getElementById("overlayResult");
   const closePopupButton = document.getElementById("popup-close");
+  const resultClosePopupButton = document.getElementById("popup-close2");
   const btnFormSubmit = document.getElementById("formSubmit");
-  const contentDiv= document.querySelector(".content");
+  const contentDiv = document.querySelector(".content");
   const myLibrary = [];
 
   //card elemanları
@@ -18,92 +20,90 @@ document.addEventListener("DOMContentLoaded", function () {
   let cardImg = document.getElementById("cardImg");
   let cardPage = document.getElementById("cardPage");
 
-// card div
-const cardDiv = document.createElement("div");
-cardDiv.classList.add("card");
+  // card div
+  const cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
 
-// card left div
-const cardLeftDiv = document.createElement("div");
-cardLeftDiv.classList.add("cardLeft");
+  // card left div
+  const cardLeftDiv = document.createElement("div");
+  cardLeftDiv.classList.add("cardLeft");
 
-// img
-cardImg = document.createElement("img");
-cardImg.classList.add("bookImg");
-cardImg.id = "cardImg";
-cardImg.src = "./img/cd.png";
-cardImg.alt = "";
-cardImg.srcset = "";
-cardLeftDiv.appendChild(cardImg);
+  // img
+  cardImg = document.createElement("img");
+  cardImg.classList.add("bookImg");
+  cardImg.id = "cardImg";
+  cardImg.src = "./img/cd.png";
+  cardImg.alt = "";
+  cardImg.srcset = "";
+  cardLeftDiv.appendChild(cardImg);
 
-// status button
-const statusButton = document.createElement("button");
-statusButton.textContent = "okundu";
-const statusDiv = document.createElement("div");
-statusDiv.classList.add("status");
-statusDiv.appendChild(statusButton);
-cardLeftDiv.appendChild(statusDiv);
+  // status button
+  const statusButton = document.createElement("button");
+  statusButton.textContent = "okundu";
+  const statusDiv = document.createElement("div");
+  statusDiv.classList.add("status");
+  statusDiv.appendChild(statusButton);
+  cardLeftDiv.appendChild(statusDiv);
 
-cardDiv.appendChild(cardLeftDiv);
+  cardDiv.appendChild(cardLeftDiv);
 
-// card right div
-const cardRightDiv = document.createElement("div");
-cardRightDiv.classList.add("cardRigth");
+  // card right div
+  const cardRightDiv = document.createElement("div");
+  cardRightDiv.classList.add("cardRigth");
 
-const cardInfoDiv1 = document.createElement("div");
-cardInfoDiv1.classList.add("cardInfo");
-const cardIcon1 = document.createElement("img");
-cardIcon1.classList.add("cardIcon");
-cardIcon1.src = "./img/book_icon.png";
-cardIcon1.alt = "";
-cardIcon1.srcset = "";
-cardTitle = document.createElement("h4");
-cardTitle.classList.add("title");
-cardTitle.id = "cardTitle";
-cardTitle.textContent = "Çizginin Dışındakiler";
-cardInfoDiv1.appendChild(cardIcon1);
-cardInfoDiv1.appendChild(cardTitle);
+  const cardInfoDiv1 = document.createElement("div");
+  cardInfoDiv1.classList.add("cardInfo");
+  const cardIcon1 = document.createElement("img");
+  cardIcon1.classList.add("cardIcon");
+  cardIcon1.src = "./img/book_icon.png";
+  cardIcon1.alt = "";
+  cardIcon1.srcset = "";
+  cardTitle = document.createElement("h4");
+  cardTitle.classList.add("title");
+  cardTitle.id = "cardTitle";
+  cardTitle.textContent = "Çizginin Dışındakiler";
+  cardInfoDiv1.appendChild(cardIcon1);
+  cardInfoDiv1.appendChild(cardTitle);
 
-const cardInfoDiv2 = document.createElement("div");
-cardInfoDiv2.classList.add("cardInfo");
-const cardIcon2 = document.createElement("img");
-cardIcon2.classList.add("cardIcon");
-cardIcon2.src = "./img/poem.png";
-cardIcon2.alt = "";
-cardIcon2.srcset = "";
-cardAuthor = document.createElement("p");
-cardAuthor.classList.add("author");
-cardAuthor.id = "cardAuthor";
-cardAuthor.textContent = "Malcolm Gladwell";
-cardInfoDiv2.appendChild(cardIcon2);
-cardInfoDiv2.appendChild(cardAuthor);
+  const cardInfoDiv2 = document.createElement("div");
+  cardInfoDiv2.classList.add("cardInfo");
+  const cardIcon2 = document.createElement("img");
+  cardIcon2.classList.add("cardIcon");
+  cardIcon2.src = "./img/poem.png";
+  cardIcon2.alt = "";
+  cardIcon2.srcset = "";
+  cardAuthor = document.createElement("p");
+  cardAuthor.classList.add("author");
+  cardAuthor.id = "cardAuthor";
+  cardAuthor.textContent = "Malcolm Gladwell";
+  cardInfoDiv2.appendChild(cardIcon2);
+  cardInfoDiv2.appendChild(cardAuthor);
 
-const cardInfoDiv3 = document.createElement("div");
-cardInfoDiv3.classList.add("cardInfo");
-const cardIcon3 = document.createElement("img");
-cardIcon3.classList.add("cardIcon");
-cardIcon3.src = "./img/document.png";
-cardIcon3.alt = "";
-cardIcon3.srcset = "";
- cardPage = document.createElement("p");
-cardPage.classList.add("page");
-cardPage.id = "cardPage";
-cardPage.textContent = "280 Sayfa";
-cardInfoDiv3.appendChild(cardIcon3);
-cardInfoDiv3.appendChild(cardPage);
+  const cardInfoDiv3 = document.createElement("div");
+  cardInfoDiv3.classList.add("cardInfo");
+  const cardIcon3 = document.createElement("img");
+  cardIcon3.classList.add("cardIcon");
+  cardIcon3.src = "./img/document.png";
+  cardIcon3.alt = "";
+  cardIcon3.srcset = "";
+  cardPage = document.createElement("p");
+  cardPage.classList.add("page");
+  cardPage.id = "cardPage";
+  cardPage.textContent = "280 Sayfa";
+  cardInfoDiv3.appendChild(cardIcon3);
+  cardInfoDiv3.appendChild(cardPage);
 
-cardRightDiv.appendChild(cardInfoDiv1);
-cardRightDiv.appendChild(cardInfoDiv2);
-cardRightDiv.appendChild(cardInfoDiv3);
+  cardRightDiv.appendChild(cardInfoDiv1);
+  cardRightDiv.appendChild(cardInfoDiv2);
+  cardRightDiv.appendChild(cardInfoDiv3);
 
-cardDiv.appendChild(cardRightDiv);
+  cardDiv.appendChild(cardRightDiv);
 
-contentDiv.appendChild(cardDiv);
+  contentDiv.appendChild(cardDiv);
 
   btnNewBook.addEventListener("click", () => {
-    console.log("tiklandi");
     openForm();
   });
-
 
   //New Book Form
   function openForm() {
@@ -111,6 +111,10 @@ contentDiv.appendChild(cardDiv);
   }
 
   //popup close button
+  resultClosePopupButton.addEventListener("click", function () {
+    overlay.style.display = "none";
+    overlayResult.style.display = "none";
+  });
   closePopupButton.addEventListener("click", function () {
     overlay.style.display = "none";
   });
@@ -126,27 +130,33 @@ contentDiv.appendChild(cardDiv);
     e.preventDefault();
     console.log(myLibrary);
 
+    overlay.style.display = "none";
+    overlayResult.style.display = "block";
+
     while (contentDiv.firstChild) {
-        contentDiv.removeChild(contentDiv.firstChild);
-      }
-      
-      // Her bir kitap için cardDiv
-      myLibrary.forEach((book) => {
-        const cardClone = cardDiv.cloneNode(true);
-      
-        const cardTitle = cardClone.querySelector(".title");
-        const cardAuthor = cardClone.querySelector(".author");
-        const cardPage = cardClone.querySelector(".page");
-        const cardImg = cardClone.querySelector(".bookImg");
-      
-        cardTitle.textContent = book.title;
-        cardAuthor.textContent = book.author;
-        cardPage.textContent = book.page;
-        cardImg.src = book.imageUrl;
-      
-        contentDiv.appendChild(cardClone);
-      });
-     
+      contentDiv.removeChild(contentDiv.firstChild);
+    }
+
+    // Her bir kitap için cardDiv
+    myLibrary.forEach((book) => {
+      const cardClone = cardDiv.cloneNode(true);
+
+      const cardTitle = cardClone.querySelector(".title");
+      const cardAuthor = cardClone.querySelector(".author");
+      const cardPage = cardClone.querySelector(".page");
+      const cardImg = cardClone.querySelector(".bookImg");
+
+      cardTitle.textContent = book.title;
+      cardAuthor.textContent = book.author;
+      cardPage.textContent = book.page;
+      cardImg.src = book.imageUrl;
+
+      contentDiv.appendChild(cardClone);
+    });
+    titleInput.value = "";
+    authorInput.value = "";
+    pageInput.value = "";
+    bookImgUrl.value = "";
   });
 });
 class Book {
